39637c9bc301847123f38c6219dbf144fc2f7fdf
Kellen Parker <kellenrparker@gmail.com>
2020-07-04 14:12:50 -0500

    about to place temporary fill in for Fight.cpp to test league settings

--- Fight.cpp
+++ Fight.cpp
@@ -1,5 +1,6 @@
 #include "Fight.h"
 #include "Fighter.h"
+#include <iostream>
 
 
 Fight::Fight(Fighter *f1, Fighter *f2)
@@ -7,6 +8,10 @@ Fight::Fight(Fighter *f1, Fighter *f2)
 
 	Fight::attendence = 0;
 	Fight::score = 0;
+
+	std::cout << "Fight!!!" << std::endl;
+
+	
 
 }
 
